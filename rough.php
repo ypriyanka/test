54052e774f9d73ce4231fca7f1f1f819655eb8c1
y.priyanka <ypriyankay@gmail.com>
2014-04-03 10:21:45 +0530

    second commit

--- rough.php
+++ rough.php
@@ -2,8 +2,8 @@
 ini_set("display_errors",1);
 $f=fopen("comment.txt","r+") or die('read');
 
-echo $data=file_get_contents("comment.txt");
-/*
+//echo $data=file_get_contents("comment.txt");
+
 $lines=array();
 $x=0;
 while(!feof($f))
@@ -12,20 +12,20 @@ $lines[$x]= fgets($f);
 $x++;  
 }
  print_r($lines);
-*/
+
 
 /*$a = explode("\n", $data); 
 foreach($a as $lines) {
     $lines = explode(' ', $lines); 
 }*/
 
-$lines= preg_split("/[\n \s]/", $data );
+//$lines= preg_split("/[\n \s]/", $data );
 
 $counter=0;
 foreach($lines as $value) 
 { 
-       //if (preg_match("(//|/\*(.|[\r\n])*?\*/)", $value)) 
-       if (preg_match("(//|/\*)", $value)) 
+       if (preg_match("(#|//|/\*(.|[\r\n])*?\*/)", $value)) 
+       //if (preg_match("(//|/\*)", $value)) 
     {
     	  	$counter++;
 			echo "<br><br>".$counter.$value."<br>";
